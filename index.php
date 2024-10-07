3b20a93452131cc48625a6c2a1e675fa4ad91d35
Dayanita196 <dayanahuaman347@gmail.com>
2024-10-06 19:22:09 -0500

    enlinea

--- index.php
+++ index.php
@@ -4,29 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
-    <link rel="stylesheet" href="resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="mains.css">
+    <!--css de la  -->
+   <?php include 'partes/css.php'; ?>
 </head>
 <body class="px-3">
-    <!-- Encabezado -->
-    <div class="row mt-5">
-        <div class="col-sm-12 col-md-6">
-            <h1 class="text-center"> EN LINEA </h1>
-
-        </div>
-        <div class="col-sm-12 col-md-6">
-            <p class="text-right pr-5">SOLUCIONES MUNDIALES</p>
-
-        </div>
-
-    </div>
-    <!-- fin de encabezado -->
-     <div class="row ">
-        <div class="col-sm-12">
-            <img class="w-100" style="height: 350px;" src="resources/img/baner01.PNG" alt="">
-        </div>
-     </div>
+    <!-- header de la pagina -->
+    <?php include 'partes/header.php'; ?>
+   
      <div class="container mt-4 ">
         <h1 class="h1">SOLUCIONES CREATIVAS</h1>
         <p>sin importar cual sea tu negocio, nuestro equipo puede hacerlo desde cero.</p>
@@ -64,9 +48,7 @@
         </div>
 
     </div>
-    <footer class="bg-wine">
-        <p class="p-5 text-white">Edificio IDEX PJ, jr. Amazonas 120 - Chachapoyas</p>
-
-    </footer>
+    
+    <?php include 'partes/footer.php'; ?>
 </body>
 </html>
